fa5568dea02234ccacfd73cf6b9a0386c9bc4613
Your Name Nontaphat003 <s6506021620083@email.kmutnb.ac.th>
2024-02-27 10:01:36 +0700

    10

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Contact from "./assets/Contact"
+import Hello from "./assets/Hello"
+import Counter from "./assets/Counter";
+
+function App() {
+
+  const helloData = [
+    {name: "Mike", surname: "Brow"},
+    {name: "Bob", surname: "Swagger"}
+  ];
+
+  return (
+    <div className='App'>
+        < Counter />
+        {helloData.map((data, index) => (
+          <Hello key={index} name={data.name} message={data.message}/>
+        ))}
+
+        <Contact email="Anirach@gmail.com" phone ="0817320731"/>
+    </div>
+  );
+}
+
+export default App
+
